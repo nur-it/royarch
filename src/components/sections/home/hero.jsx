2f575e7c68695ca68da68e1024c5d4d905a7bb60
Al Amin <alamincoders@gmail.com>
2025-08-09 11:44:30 +0600

    config: custom tailwind styles

--- src/components/sections/home/hero.jsx
+++ src/components/sections/home/hero.jsx
@@ -1,9 +1,5 @@
 const HeroSection = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-amber-700">Hello world!</h1>
-    </div>
-  );
+  return <div>HeroSection</div>;
 };
 
 export default HeroSection;
