5ddf5b34ead104a12bb4230bdc4613c6a546b2a7
Al Amin <alamincoders@gmail.com>
2025-08-11 10:06:14 +0600

    add: menu on the layout

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,12 +2,13 @@
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 import Logo from "./Logo";
+import MenuItems from "./menu-items";
 import Sidebar from "./sidebar";
 
 const HeaderSection = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <>
+    <div className="relative">
       <div
         className={cn(
           "bg-night fixed top-0 left-25 z-50 flex h-25 w-full items-center pl-4 md:pl-8 lg:w-[310px] lg:justify-center lg:pl-0",
@@ -20,7 +21,11 @@ const HeaderSection = () => {
       <aside className="fixed top-0 left-0 z-40 w-25 lg:h-screen">
         <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       </aside>
-    </>
+
+      <div className="fixed top-25 z-40 w-full lg:left-25 lg:w-[400px]">
+        <MenuItems />
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/layout/menu-items.jsx
@@ -0,0 +1,11 @@
+const MenuItems = () => {
+  return (
+    <div className="bg-quaternary text-white shadow">
+      <div className="custom_scrollbar max-h-[calc(100vh-100px)] overflow-y-auto px-[30px] py-10">
+        MenuItems
+      </div>
+    </div>
+  );
+};
+
+export default MenuItems;
